54e9a2c257e9d8c204944f9126c32c773e08995e
Eita <kusaiyuka@gmail.com>
2026-03-23 18:39:20 +0900

    add(logout): logout画面を追加

---
+++ app/pages/logout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui';
+import { z } from 'zod';
+import { authClient } from '~/composable/auth-client';
+import { useTurnstile } from '~/composable/useTurnstile';
+
+definePageMeta({
+  layout: 'the-front',
+});
+
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center justify-center gap-4">
+      <img src="/pitamai-only-logo.png" class="h-12" alt="PitaMai Logo" />
+      <p class="text-xl font-semibold">共通アカウント</p>
+    </div>
+    <div class="flex flex-col items-center space-y-4 py-8">
+      <p>連携先アプリケーションでログアウトしました。</p>
+    </div>
+  </div>
+</template>
