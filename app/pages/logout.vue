b74ccae25c09e6fc8a67b2946aa4c6fd4de0c596
Eita <kusaiyuka@gmail.com>
2026-03-24 13:00:59 +0900

    add(logout): ログアウトページをクエリにより、メッセージを変えるようにした

--- app/pages/logout.vue
+++ app/pages/logout.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import type { FormSubmitEvent } from '@nuxt/ui';
-import { z } from 'zod';
-import { authClient } from '~/composable/auth-client';
-import { useTurnstile } from '~/composable/useTurnstile';
-
 definePageMeta({
   layout: 'the-front',
 });
 
+const route = useRoute();
+const messageCode = computed(() => route.query.messages as string);
 </script>
 
 <template>
@@ -16,8 +13,23 @@ definePageMeta({
       <img src="/pitamai-only-logo.png" class="h-12" alt="PitaMai Logo" />
       <p class="text-xl font-semibold">共通アカウント</p>
     </div>
-    <div class="flex flex-col items-center space-y-4 py-8">
-      <p>連携先アプリケーションでログアウトしました。</p>
+    <div class="flex items-center justify-center p-4">
+      <UPageCard class="max-w-5xl">
+        <div class="flex flex-col items-center space-y-6 py-8">
+          <template v-if="messageCode === 'M001'">
+            <p class="text-lg">PitaMaiアカウントからログアウトしました。</p>
+            <UButton to="/login" color="primary">
+              再ログイン
+            </UButton>
+          </template>
+          <template v-else-if="messageCode === 'M002'">
+            <p class="text-lg">連携先アプリケーションでログアウトしました。</p>
+          </template>
+          <template v-else>
+            <p class="text-lg">ログアウトしました。</p>
+          </template>
+        </div>
+      </UPageCard>
     </div>
   </div>
 </template>
