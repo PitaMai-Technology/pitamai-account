fd515db91795ad97de2b7d162d84f65d7fe74b87
Eita <kusaiyuka@gmail.com>
2026-05-25 00:01:13 +0900

    fix(エラー画面): エラー画面に画像を追加

--- app/pages/error.vue
+++ app/pages/error.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <h1>エラー！</h1>
+    <h1 class="mb-8">エラー！</h1>
+    <img src="/error.png" alt="Error Image" class="bg-gray-500/80 w-64 h-auto p-4" />
   </div>
 </template>
 
